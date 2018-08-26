7024026e6a7b36c9848689e7baf857f04e045bbc
LAPTOP-M15GP1N5\aryexo <1205410385@qq.com>
2018-08-26 16:00:40 +0800

    数组去重

--- src/com/test/base/Test03.java
+++ src/com/test/base/Test03.java
@@ -1,6 +1,73 @@
 package com.test.base;
 
+import java.util.HashMap;
+
 //数据组重复的数字
 public class Test03 {
+    public static void main(String[] args){
+        int[] numbers = {2,3,1,0,2,5,3};
+        dup(numbers);
+    }
+
+    public static boolean dup(int[] numbers){
+        if(numbers==null||numbers.length==0){
+            return false;
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<numbers.length;i++){
+            if(map.containsValue(numbers[i])){
+                System.out.print(numbers[i]);
+                return true;
+            }else{
+                map.put(i,numbers[i]);
+            }
+
+        }
+        return false;
+    }
+
+    public boolean dup2(int[] numbers){
+        if(numbers==null||numbers.length==0){
+            return false;
+        }
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] < 1 || numbers[i] > numbers.length-1)
+                return false;
+        }
+        int low=1;
+        int high=numbers.length-1;
+        while (low<=high){
+            int mid = (low+high)/2;
+            int count = 0;
+            //对范围内的元素个数进行计数
+            for(int i = 0; i < numbers.length; i++)
+            {
+                if(numbers[i] <= mid && numbers[i] >= low)
+                    count++;
+            }
+
+            if(low == high)
+            {
+                if(count > 1) {
+
+                    System.out.print(low);
+                    return true;
+                }
+                else
+                    break;
+            }
+
+            // 根据检测情况调整会出现重复的整数范围
+            if(count > mid - low + 1)
+                high = mid;
+            else
+                low = mid + 1;
+
+        }
+
+        return false;
+
+
+    }
 
 }
